594061f90fd61744a4eae84e102c8772a18af57c
Jeffrey Skonhovd <jskonhovd@gmail.com>
2014-02-27 11:03:56 -0600

    Checkin

---
+++ Lightning.jl
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+A=rand(5,5)
+
+# <codecell>
+
+A[1,1]
+
+# <codecell>
+
+rand(5,5)[1,1]
+
+# <codecell>
+
+[i for i=1:5]
+
+# <codecell>
+
+[trace(rand(n,n)) for n=1:5]
+
+# <codecell>
+
+x=rand(10); [x[i] + x[i+1] for i=1:9]
+
+# <codecell>
+
+
